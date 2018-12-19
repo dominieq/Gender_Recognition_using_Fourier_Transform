e94d6423b320892342ba26e258c682d140681b51
vonshick <kubawasik@vp.pl>
2018-12-19 14:28:18 +0100

    Some initial imports

--- inf132326_inf132335.py
+++ inf132326_inf132335.py
@@ -5,9 +5,27 @@ from scipy import *
 from ipywidgets import *
 import math as mt
 import scipy.io.wavfile
+import sys
+from scipy.signal import decimate
 
-w_k, signal_k = scipy.io.wavfile.read('trainall/001_K.wav')
-w_m, signal_m = scipy.io.wavfile.read('trainall/002_M.wav')
+def main():
+    # wav_file = sys.argv[1]
+    for i in range(2,10,1):
+        # w_m, signal_m = scipy.io.wavfile.read('trainall/002_M.wav')
+        print(i)
+        k_czy_m = ''
+        if i < 10:
+            try:
+                w, signal = scipy.io.wavfile.read('trainall/00'+repr(i)+'_K.wav')
+            except:
+                w, signal = scipy.io.wavfile.read('trainall/00'+repr(i)+'_M.wav')
+        else:
+            try:
+                w, signal = scipy.io.wavfile.read('trainall/0'+repr(i)+'_K.wav')
+            except:
+                w, signal = scipy.io.wavfile.read('trainall/0'+repr(i)+'_M.wav')
 
-print(w_k)
 
+    # print(w_k)
+
+main()
