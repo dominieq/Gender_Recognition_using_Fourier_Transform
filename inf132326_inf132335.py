f35932c6e16890d507d986fec8fffbff5be75a72
vonshick <kubawasik@vp.pl>
2018-12-19 14:39:39 +0100

    Function add but not work yet

--- inf132326_inf132335.py
+++ inf132326_inf132335.py
@@ -8,6 +8,15 @@ import scipy.io.wavfile
 import sys
 from scipy.signal import decimate
 
+def find_main_signal(signal):
+    signals = []
+    main_signal = []
+    for i in range(2,5):
+      #chyba nie ta funkcja
+        # signals.append(decimate(signal, i))
+
+    return main_signal
+
 def main():
     # wav_file = sys.argv[1]
     for i in range(2,10,1):
@@ -17,15 +26,17 @@ def main():
         if i < 10:
             try:
                 w, signal = scipy.io.wavfile.read('trainall/00'+repr(i)+'_K.wav')
+                k_czy_m = 'K'
             except:
                 w, signal = scipy.io.wavfile.read('trainall/00'+repr(i)+'_M.wav')
+                k_czy_m = 'M'
         else:
             try:
                 w, signal = scipy.io.wavfile.read('trainall/0'+repr(i)+'_K.wav')
+                k_czy_m = 'K'
             except:
                 w, signal = scipy.io.wavfile.read('trainall/0'+repr(i)+'_M.wav')
-
-
-    # print(w_k)
-
+                k_czy_m = 'M'
+        print('W pliku: '+k_czy_m)
+        main_signal = find_main_signal(signal)
 main()
