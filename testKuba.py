7f64ae2df1199ed3484ce6980de98ecb2c36649b
Dominik <dominik.szmyt@student.put.poznan.pl>
2018-12-27 14:22:07 +0100

    Correct files 78/89 (87%)

--- testKuba.py
+++ testKuba.py
@@ -12,27 +12,20 @@ from scipy.signal import decimate
 def find_main_signal(signal):
     processed_signal = signal
     decimated = []
-    # process_depth = 6
-    # for i in range(2, 2+process_depth):
-    #     decimated.append(decimate(signal, i))
-    # 'decimate' function gives us twice smaller array than the one from input
-    # we cut from the arrays all elements which index is greater than the length of last array 
-    # signals_length = len(decimated[process_depth-1])
-    # processed_signal = processed_signal[:signals_length]
-    # for dec in decimated:
-    #     processed_signal = processed_signal * dec[:signals_length]
-    for i in range(2, 6):
+    for i in range(1, 7):
         decimated.append(decimate(signal, i))
-    for i in range(len(decimated[3])):
-        processed_signal[i] = processed_signal[i] * decimated[0][i] * decimated[1][i] * decimated[2][i] * decimated[3][i]
+
+    for i in range( len(decimated)):
+        for j in range( len(decimated[-1]) ):
+            processed_signal[j] = processed_signal[j] * decimated[i][j]
+
     for i in range(len(decimated[3]), len(processed_signal)):
         processed_signal[i] = 0
     return processed_signal
 
 def main():
     # wav_file = sys.argv[1]
-    ##############################################################################
-    czary = np.array([1,2,3,4])
+
     kobiety = []
     faceci = []
     correct_recognitions = 0
@@ -55,15 +48,14 @@ def main():
             except:
                 w, signal = scipy.io.wavfile.read('trainall/0'+repr(i)+'_M.wav')
                 correct_gender = 'M'
-        print('W pliku: '+correct_gender)
+        
+        print('W pliku: ' + correct_gender)
         if(len(signal.shape) != 1): # niektóre dźwięki mają dwa kanaly- wybieramy jeden
             signal = [ x[0] for x in signal ]
-            # signal = [ x[1] for x in signal ]
-            # signal = [ mean(x) for x in signal ]
         
         fft_signal = abs(fft(signal))
         main_signal = find_main_signal(fft_signal)
-        amp_max = max(main_signal[200:])#odcinamy te początkowe, bo tam zazwyczaj są nieinteresujące nas wartości
+        amp_max = max(main_signal[200:]) # odcinamy te początkowe, bo tam zazwyczaj są nieinteresujące nas wartości
 
         n = len(main_signal)
         for i in range(len(main_signal[200:])): 
@@ -73,20 +65,13 @@ def main():
                     recognized_gender = 'M'
                 else:
                     recognized_gender = 'K'
-                print('Rozpoznana: '+recognized_gender)
-                print('Index: '+repr(i)+' value: '+repr(f))
+                print('Rozpoznana: ' + recognized_gender)
+                print('Index: ' + repr(i) + ' value: ' +repr(f))
 
         if recognized_gender == correct_gender:
             correct_recognitions += 1
 
-    print('Skuteczność: '+repr(correct_recognitions/89)) #89, bo sprawdzam wszystkie pliki z wyjątkiem 1 i 8
-        # freqs = range(int(len(main_signal)))
-        # freqs = [(x / len(main_signal)) * w for x in freqs]
-        # plt.stem(freqs, main_signal, '-*')
-    # print("################# kobiety #################")
-    # print(kobiety)
-    # print("################# faceci #################")
-    # print(faceci)
+    print('Skuteczność ' + repr(correct_recognitions) + '/89: ' + repr(correct_recognitions / 89)) # 89, bo sprawdzam wszystkie pliki z wyjątkiem 1 i 8
 
 
 main()
